7c6f8c16d0c74f5f95a056ebc963b4e146da1b9a
muhammad risano <muhammadrisano@gmail.com>
2024-08-24 15:36:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// getElementById => return sebuah element
+// cara untuk meseleksi ketika elementnya memiliki atribu id
+
+const elH1 = document.getElementById("title")
+// console.log(elH1);
+
+// getElementsByClassName => html collection
+
+const elLi = document.getElementsByClassName('item')
+// console.log(elLi);
+
+
+// getElementsByTagName => html collection
+
+const elP =document.getElementsByTagName('p')
+// console.log(elP);
+
+
+// querySelector => element
+
+// const elSubTitle = document.querySelector('.sub-title')
+// const elTitle = document.querySelector('#title')
+// console.log(elSubTitle);
+
+
+// querySelectorAll = nodelist
+
+const elSubTitle2 = document.querySelectorAll('.sub-title')
+// console.log(elSubTitle2);
